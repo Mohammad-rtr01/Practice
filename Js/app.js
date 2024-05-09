6cf1c3cb109e352e1b7e914d89b9cd862011cb3f
mohammad rtr <assd13@gmail.com>
2024-05-09 14:13:19 -0700

    finish

---
+++ Js/app.js
@@ -0,0 +1,12 @@
+alert("Enter your informations") ;
+
+let name = prompt("what's your name?") ;
+let age = prompt("How old are you") ;
+let areYouMarried = confirm("are you married") ;
+let job = prompt("What's your job") ;
+let = 6 ;
+console.log(name) ;
+console.log(age) ;
+console.log(areYouMarried) ;
+console.log(job) ;
+console.log(let)
